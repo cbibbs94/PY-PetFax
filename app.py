f45afd141d26326833a3f3c0fa0e4dc82e2b7c7c
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-05-04 16:23:13 -0700

    created factory and app instance

--- app.py
+++ app.py
@@ -1,12 +1,2 @@
-from flask import Flask
-app = Flask(__name__)
-
-#index route
-@app.route('/')
-def index():
-    return 'Hello, this is PetFAx!'
-
-#pets route
-@app.route('/pets')
-def pets():
-    return "These are our pets available for adoption"
+from petfax import create_app
+app = create_app()
