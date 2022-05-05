8bba36bfcbd52c5fbf7d72ac926690308c294746
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-05-04 20:29:32 -0700

    added database

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 #Factory
 def create_app():
     app = Flask(__name__)
+
+    #Postgres Connection
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Kyuubichan15!@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False             
+
+    from . import models
+    models.db.init_app(app)
     
     #index route
     @app.route('/')
@@ -13,6 +21,7 @@ def create_app():
     #register pet blueprint
     from . import pet
     app.register_blueprint(pet.bp)
+    migrate = Migrate(app, models.db)
 
 
     from . import fact
